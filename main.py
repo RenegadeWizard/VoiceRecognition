491bcb1ba99a2831708df7177cc8125f78a651b8
Krzysztof Sychla <ksychla5@gmail.com>
2019-12-11 18:54:10 +0100

    Add audio files

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+import scipy.io.wavfile
 
 
-def main():
-    pass
+def main(audio):
+    fs, audio = scipy.io.wavfile.read(audio)
 
 
-if __name__=='__main__':
-    main()
+if __name__ == '__main__':
+    main(sys.argv[1])
