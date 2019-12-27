efb7f59c6cfdc077dd25e1ecea83689ace0c798f
Damian Wędzikowski <dwedzikowski@gmail.com>
2019-12-27 17:26:00 +0100

    Somehow working

--- main.py
+++ main.py
@@ -12,27 +12,10 @@ def spectral_properties(y: np.ndarray, fs: int):
     mean = (freq * amp).sum()
     sd = np.sqrt(np.sum(amp * ((freq - mean) ** 2)))
     amp_cumsum = np.cumsum(amp)
-    # median = freq[len(amp_cumsum[amp_cumsum <= 0.5]) + 1]
-    # mode = freq[amp.argmax()]
-    Q25 = freq[len(amp_cumsum[amp_cumsum <= 0.25]) + 1]
-    Q75 = freq[len(amp_cumsum[amp_cumsum <= 0.75]) + 1]
-    IQR = Q75 - Q25
-    # z = amp - amp.mean()
-    # w = amp.std()
-    # skew = ((z ** 3).sum() / (len(spec) - 1)) / w ** 3
-    # kurt = ((z ** 4).sum() / (len(spec) - 1)) / w ** 4
-    # result_d = {
-    #     'mean': mean,
-    #     'sd': sd,
-    #     'median': median,
-    #     'mode': mode,
-    #     'Q25': Q25,
-    #     'Q75': Q75,
-    #     'IQR': IQR,
-    #     'skew': skew,
-    #     'kurt': kurt
-    # }
-    return Q25, IQR, sd
+    q25 = freq[len(amp_cumsum[amp_cumsum <= 0.25]) + 1]
+    q75 = freq[len(amp_cumsum[amp_cumsum <= 0.75]) + 1]
+    iqr = q75 - q25
+    return q25, iqr, sd
 
 
 def main():
@@ -44,29 +27,30 @@ def main():
         if len(data.shape) > 1:
             data = data[:, 0]
         t = np.arange(1024, len(data), 256)
-        sum = 0
-        fft = []
-        freq = range(80, 256)
+        _sum = 0
 
         for i in range(len(t)):
-
             data_slice = data[(t[i]-1024):t[i]]
             pre_fft = np.fft.fft(data_slice)
             fft = abs(pre_fft[40:256])
-
-            sum += np.argmax(fft)
+            _sum += np.argmax(fft)
 
         q25, iqr, sd = spectral_properties(data, fs)
-        result = (sum/len(t))
-        print(file + " : " + str(result))
+        result = (_sum/len(t))
+        print(file + ", FreqResult: " + str(result) + ", IQR-SD: " + str(iqr-sd))
         r_threshold = 60.0
         i_threshold = 100
         if (result < r_threshold or (iqr-sd) < i_threshold) and file[4] == 'M':
             successful += 1
+            print('Recognized: Male\n')
 
-        if (result > r_threshold or (iqr-sd) > i_threshold) and file[4] == 'K':
+        elif (result > r_threshold or (iqr-sd) > i_threshold) and file[4] == 'K':
             successful += 1
-    print(str(100*successful/len(onlyfiles))+'%')
+            print('Recognized: Female\n')
+        else:
+            print('FAILED TO RECOGNIZE GENDER!\n')
+
+    print('Success rate of gender recognition: ' + str(100*successful/len(onlyfiles))+'%')
 
 
 if __name__ == '__main__':
