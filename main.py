2de3117f248fb09648ce13242da9849c31d6697e
Damian Wędzikowski <dwedzikowski@gmail.com>
2019-12-27 16:38:11 +0100

    I don't know

--- main.py
+++ main.py
@@ -37,12 +37,13 @@ def spectral_properties(y: np.ndarray, fs: int):
 
 def main():
     onlyfiles = [f for f in listdir("trainall/") if isfile(join("trainall/", f))]
+    successful = 0
 
     for file in onlyfiles:
         fs, data = scipy.io.wavfile.read("trainall/" + file)
         if len(data.shape) > 1:
             data = data[:, 0]
-        t = np.arange(1024, len(data), 512)
+        t = np.arange(1024, len(data), 256)
         sum = 0
         fft = []
         freq = range(80, 256)
@@ -51,12 +52,20 @@ def main():
 
             data_slice = data[(t[i]-1024):t[i]]
             pre_fft = np.fft.fft(data_slice)
-            fft = abs(pre_fft[80:256])
+            fft = abs(pre_fft[40:256])
 
             sum += np.argmax(fft)
 
         q25, iqr, sd = spectral_properties(data, fs)
-        print(file + " : " + str((sum/len(t))))
+        result = (sum/len(t))
+        print(file + " : " + str(result))
+        threshold = 60.0
+        if (result < threshold or (iqr-sd) < 0) and file[4] == 'M':
+            successful += 1
+
+        if (result > threshold or (iqr-sd) > 0) and file[4] == 'K':
+            successful += 1
+    print(str(100*successful/len(onlyfiles))+'%')
 
 
 if __name__ == '__main__':
